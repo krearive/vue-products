dc1c3d96246b901ce3750a9fc9bd32edb91c4169
krearive <arifblogger77@gmail.com>
2025-05-14 21:43:35 +0700

    feat: add product card component vp-1

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,46 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, computed, onMounted } from "vue";
+import ProductCard from "./components/ProductCard.vue";
+
+const products = ref([])
+const search = ref("")
+const cart = ref([])
+
+onMounted(async () => {
+  const response = await fetch("https://fakestoreapi.com/products");
+  products.value = await response.json();
+})
+
+const filteredProducts = computed(() => {
+  return products.value.filter(product => {
+    return product.title.toLowerCase().includes(search.value.toLowerCase());
+  });
+})
+
+function addToCart(product) {
+  const existingProduct = cart.value.find(item => item.id === product.id);
+  if (existingProduct) {
+    existingProduct.quantity++;
+  } else {
+    cart.value.push({ ...product, quantity: 1 });
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="border-2 font-inter">
+    <header class="flex justify-between items-center p-4 border-b-2">
+      <h1 class="text-xl font-bold">Vue Store</h1>
+      <div class="bg-black text-white px-3 py-1 rounded hover:shadow-(--shadow-pink)">
+        Cart: {{ cart.length }} {{ cart.length === 1 ? "item" : "items" }}
+      </div>
+    </header>
+
+    <input type="text" placeholder="Search products..." v-model="search"
+      class="border-b-2 p-2 mb-4 w-full focus:outline" />
+
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+      <ProductCard v-for="product in filteredProducts" :key="product.id" :product="product" @add-to-cart="addToCart" />
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,28 @@
+<script setup>
+defineProps(['product'])
+</script>
+
+<template>
+  <div class="border rounded-lg p-4 hover:shadow-(--shadow-black) relative">
+    <div class="flex flex-col justify-between h-full">
+      <div>
+        <img :src="product.image" :alt="product.title" class="h-40 w-full object-contain mb-2" loading="lazy" />
+        <div class="flex flex-row justify-between items-start gap-2">
+          <div class="flex flex-col justify-between">
+            <h2 class="text-base font-semibold">{{ product.title }}</h2>
+            <p class="text-gray-600 text-sm capitalize">{{ product.category }}</p>
+          </div>
+          <p class="text-white bg-pink-500 shadow-(--shadow-black) rounded-full px-2">${{ product.price }}</p>
+        </div>
+        <p class="text-black absolute top-2 bg-white border rounded-full px-2 text-sm shadow-(--shadow-black)">
+          ⭐ {{ product.rating.rate }} ({{ product.rating.count }})
+        </p>
+      </div>
+
+      <button @click="$emit('add-to-cart', product)"
+        class="mt-2 text-base lg:text-sm bg-pink-500 text-white px-3 py-1 rounded hover:bg-pink-600 hover:shadow-(--shadow-pink) w-full">
+        Add to Cart
+      </button>
+    </div>
+  </div>
+</template>
